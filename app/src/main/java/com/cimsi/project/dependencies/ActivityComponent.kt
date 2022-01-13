8f853826e6f25fac7625bd57f190fcfd628e4f93
Juan Diego <39896844+juandiegovqdev@users.noreply.github.com>
2022-01-13 01:59:44 +0100

    Update ActivityComponent.kt

--- app/src/main/java/com/cimsi/project/dependencies/ActivityComponent.kt
+++ app/src/main/java/com/cimsi/project/dependencies/ActivityComponent.kt
@@ -1,9 +1,13 @@
 package com.cimsi.project.dependencies
 
+import com.cimsi.project.ui.activities.FavoriteStationsActivity
 import com.cimsi.project.ui.activities.MainActivity
+import com.cimsi.project.ui.activities.SignInActivity
 import dagger.Component
 
 @Component(modules = [BikesApiModule::class, ActivityModule::class])
 interface ActivityComponent {
     fun inject(activity: MainActivity)
+    fun inject(favoriteStationsActivity: FavoriteStationsActivity)
+    fun inject(signInActivity: SignInActivity)
 }
